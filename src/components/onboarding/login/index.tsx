2384c9e922ce900241a23da50c922842e8cb7f50
sanya-saini <sanya.saini@netsolutions.com>
2023-03-10 13:35:01 +0530

    second commit

--- src/components/onboarding/login/index.tsx
+++ src/components/onboarding/login/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, View, Pressable } from "react-native";
 import {useNavigation} from '@react-navigation/native'; // jis page se navigate krna h, uss page pr usenavigation hook import krni h
 import { styles } from "./styles";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Login = () => {
     const navigation = useNavigation(); // variable for hook
@@ -9,12 +10,12 @@ const Login = () => {
         navigation.navigate('signup'); // jis page pr jana h
     };
     return (
-        <View>
+        <SafeAreaView>
             <Text style={styles.txt}>Login Page</Text>
             <Pressable style={styles.btn} onPress={onClick}>
             <Text style={styles.txt1}>To Signup</Text>
             </Pressable>
-        </View>
+        </SafeAreaView>
 
     )
 
